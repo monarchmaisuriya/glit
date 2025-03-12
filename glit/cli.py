0005dfa1b78546783e4a8e6b210c069b13a3f793
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2025-03-12 12:36:35 +0530

    add arg parse capabilities

--- glit/cli.py
+++ glit/cli.py
@@ -1,2 +1,27 @@
-def main ():
-    print ('Hello, World!')
+import argparse
+def main():
+    """
+    Main entry point for the script.
+    """
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    """
+    Parse command line arguments using argparse.
+
+    Returns:
+        argparse.Namespace: arguments parsed from the command line
+    """
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+    return parser.parse_args()
+
+def init(args):
+    """
+    Initialize function.
+    """
+    print('Hello, World!')
